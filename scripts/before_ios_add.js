f37c947a1dced93d3e6a69c9051a21e38da74155
Zhengz <zhengz@masn.cn>
2015-08-25 17:51:09 +0800

    clear dirctory before platform add

--- scripts/before_ios_add.js
+++ scripts/before_ios_add.js
@@ -8,50 +8,70 @@ module.exports = function(context){
 	var pluginXml = path.join(pluginPath, 'plugin.xml');
 	var filesPath = path.join(pluginPath, 'src', 'ios');
 	
-	var paths = [];
+	var clearPath = function(){
+		console.info('clearing the resources directory...');
+		if(fs.existsSync(filesPath)){
+			var paths = [];
+			paths = fs.readdirSync(filesPath);
+			for(var i = 0; i < paths.length; i++){
+				fs.unlinkSync(path.join(filesPath, paths[i]));
+			}
+		}else{
+			fs.mkdirSync(filesPath);
+		}
+	};
 	
-	var configPath = path.join(projectRoot, 'config.json');
-	if(fs.existsSync(configPath)){
-		var config = JSON.parse(fs.readFileSync(configPath, {encoding: 'utf8'}));
-		if(config.RESOURCES){
-			console.info('copying file to plugin...');
-			for(var i = 0; i < config.RESOURCES.length; i++){
-				if(fs.existsSync(config.RESOURCES[i])){
-					paths = fs.readdirSync(path.join(projectRoot, config.RESOURCES[i]));
-					if(paths.length > 0){
-						for(var j = 0; j < paths.length; j++){
-							if(paths[j].indexOf('.') !== 0){
-								console.info('copying ' + path.join(projectRoot, config.RESOURCES[i], paths[j]) + ' to ' + path.join(filesPath, paths[j]));
-								var target = path.join(filesPath, paths[j]);
-								var origin = path.join(projectRoot, config.RESOURCES[i], paths[j]);
-								fs.writeFileSync(target, fs.readFileSync(origin));
-								// pathContents.push(TEMPLATE.replace('{{path}}', f));
+	var copyFiles = function(){
+		var configPath = path.join(projectRoot, 'config.json');
+		if(fs.existsSync(configPath)){
+			var config = JSON.parse(fs.readFileSync(configPath, {encoding: 'utf8'}));
+			if(config.RESOURCES){
+				console.info('copying file to plugin...');
+				for(var i = 0; i < config.RESOURCES.length; i++){
+					if(fs.existsSync(config.RESOURCES[i])){
+						var paths = fs.readdirSync(path.join(projectRoot, config.RESOURCES[i]));
+						if(paths.length > 0){
+							for(var j = 0; j < paths.length; j++){
+								if(paths[j].indexOf('.') !== 0){
+									console.info('copying ' + path.join(projectRoot, config.RESOURCES[i], paths[j]) + ' to ' + path.join(filesPath, paths[j]));
+									var target = path.join(filesPath, paths[j]);
+									var origin = path.join(projectRoot, config.RESOURCES[i], paths[j]);
+									fs.writeFileSync(target, fs.readFileSync(origin));
+									// pathContents.push(TEMPLATE.replace('{{path}}', f));
+								}
 							}
 						}
 					}
 				}
 			}
 		}
-	}
+	};
 	
-	if(fs.existsSync(filesPath)){
-		paths = fs.readdirSync(filesPath);
-	}
-	
-	var pathContents = [];
-	if(paths.length > 0){
-		console.info('setting plugin resources...');
-		for(var i = 0; i < paths.length; i++){
-			var file = ['src', '/ios/', paths[i]].join('');
-			pathContents.push(TEMPLATE.replace('{{path}}', file));
+	var settingPlugin = function(){
+		var paths = [];
+		if(fs.existsSync(filesPath)){
+			paths = fs.readdirSync(filesPath);
 		}
-	}
+		var pathContents = [];
+		if(paths.length > 0){
+			console.info('setting plugin resources...');
+			for(var i = 0; i < paths.length; i++){
+				if(paths[i].indexOf('.') !== 0){
+					var file = ['src', '/ios/', paths[i]].join('');
+					pathContents.push(TEMPLATE.replace('{{path}}', file));
+				}
+			}
+		}
+		var pluginContent = fs.readFileSync(pluginXml, {encoding: 'utf8'});
+		if(pluginContent.indexOf('<!--resources-->') >= 0 && pluginContent.indexOf('<!--endresources-->') >= 0){
+			var contentStart = [pluginContent.split('<!--resources-->')[0], '<!--resources-->'].join('');
+			var contentEnd = ['<!--endresources-->', pluginContent.split('<!--endresources-->')[1]].join('');
+			pluginContent = [contentStart, pathContents.join('\n'), contentEnd].join('\n');
+		}
+		fs.writeFileSync(pluginXml, pluginContent);
+	};
 	
-	var pluginContent = fs.readFileSync(pluginXml, {encoding: 'utf8'});
-	if(pluginContent.indexOf('<!--resources-->') >= 0 && pluginContent.indexOf('<!--endresources-->') >= 0){
-		var contentStart = [pluginContent.split('<!--resources-->')[0], '<!--resources-->'].join('');
-		var contentEnd = ['<!--endresources-->', pluginContent.split('<!--endresources-->')[1]].join('');
-		pluginContent = [contentStart, pathContents.join('\n'), contentEnd].join('\n');
-	}
-	fs.writeFileSync(pluginXml, pluginContent);
+	clearPath();
+	copyFiles();
+	settingPlugin();
 };

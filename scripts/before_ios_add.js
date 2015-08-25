a8542707df7e882b84b436217f3bc782140fae89
Zhengz <zhengz@masn.cn>
2015-08-25 17:19:01 +0800

    fix bug

--- scripts/before_ios_add.js
+++ scripts/before_ios_add.js
@@ -9,6 +9,29 @@ module.exports = function(context){
 	var filesPath = path.join(pluginPath, 'src', 'ios');
 	
 	var paths = [];
+	
+	var configPath = path.join(projectRoot, 'config.json');
+	if(fs.existsSync(configPath)){
+		var config = JSON.parse(fs.readFileSync(configPath, {encoding: 'utf8'}));
+		if(config.RESOURCES){
+			for(var i = 0; i < config.RESOURCES.length; i++){
+				if(fs.existsSync(config.RESOURCES[i])){
+					paths = fs.readdirSync(config.RESOURCES[i]);
+					if(paths.length > 0){
+						for(var j = 0; j < paths.length; j++){
+							if(paths[j] !== '.gitkeep'){
+								var target = path.join(filesPath, paths[j]);
+								var origin = path.join(config.RESOURCES[i], paths[j]);
+								fs.writeFileSync(target, fs.readFileSync(origin));
+								// pathContents.push(TEMPLATE.replace('{{path}}', f));
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	
 	if(fs.existsSync(filesPath)){
 		paths = fs.readdirSync(filesPath);
 	}
@@ -21,28 +44,10 @@ module.exports = function(context){
 		}
 	}
 	
-	var configPath = path.join(projectRoot, 'config.json');
-	if(fs.existsSync(configPath)){
-		var config = JSON.parse(fs.readdirSync(configPath, {encoding: 'utf8'}));
-		if(config.RESOURCES){
-			for(var i = 0; i < config.RESOURCES.length; i++){
-				if(fs.existsSync(config.RESOURCES[i])){
-					paths = fs.readdirSync(config.RESOURCES[i]);
-					if(paths.length > 0){
-						for(var i = 0; i < paths.length; i++){
-							var f = ['src', '/ios/', paths[i]].join('');
-							pathContents.push(TEMPLATE.replace('{{path}}', f));
-						}
-					}
-				}
-			}
-		}	
-	}
-	
 	var pluginContent = fs.readFileSync(pluginXml, {encoding: 'utf8'});
 	if(pluginContent.indexOf('<!--resources-->') >= 0 && pluginContent.indexOf('<!--endresources-->') >= 0){
 		var contentStart = [pluginContent.split('<!--resources-->')[0], '<!--resources-->'].join('');
-		var contentEnd = [pluginContent.split('<!--endresources-->')[1], '<!--endresources-->'].join('');
+		var contentEnd = ['<!--endresources-->', pluginContent.split('<!--endresources-->')[1]].join('');
 		pluginContent = [contentStart, pathContents.join('\n'), contentEnd].join('\n');
 	}
 	fs.writeFileSync(pluginXml, pluginContent);

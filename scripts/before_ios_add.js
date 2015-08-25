f1dcf465eccc52d960ab19da212d8e00f664a8d1
Zhengz <zhengz@masn.cn>
2015-08-25 11:03:26 +0800

    use config.json to get resources dir

--- scripts/before_ios_add.js
+++ scripts/before_ios_add.js
@@ -7,22 +7,43 @@ module.exports = function(context){
 	var pluginPath = path.join(projectRoot, 'plugins', 'com.mobishift.plugins.IOSResources');
 	var pluginXml = path.join(pluginPath, 'plugin.xml');
 	var filesPath = path.join(pluginPath, 'src', 'ios');
-	var paths = fs.readdirSync(filesPath);
 	
-	console.info('there are ' + paths.length + 'resources');
+	var paths = [];
+	if(fs.existsSync(filesPath)){
+		paths = fs.readdirSync(filesPath);
+	}
+	
+	var pathContents = [];
 	if(paths.length > 0){
-		var pathContents = [];
 		for(var i = 0; i < paths.length; i++){
-			var file = ['src', 'ios', paths[i]].join('');
+			var file = ['src', '/ios/', paths[i]].join('');
 			pathContents.push(TEMPLATE.replace('{{path}}', file));
 		}
-		
-		var pluginContent = fs.readFileSync(pluginXml, {encoding: 'utf8'});
-		if(pluginContent.indexOf('<!--resources-->') >= 0 && pluginContent.indexOf('<!--endresources-->') >= 0){
-			var contentStart = [pluginContent.split('<!--resources-->')[0], '<!--resources-->'].join('');
-			var contentEnd = [pluginContent.split('<!--endresources-->')[1], '<!--endresources-->'].join('');
-			pluginContent = [contentStart, pathContents.join('\n'), contentEnd].join('\n');
-		}
-		fs.writeFileSync(pluginXml, pluginContent);
 	}
+	
+	var configPath = path.join(projectRoot, 'config.json');
+	if(fs.existsSync(configPath)){
+		var config = JSON.parse(fs.readdirSync(configPath, {encoding: 'utf8'}));
+		if(config.RESOURCES){
+			for(var i = 0; i < config.RESOURCES.length; i++){
+				if(fs.existsSync(config.RESOURCES[i])){
+					paths = fs.readdirSync(config.RESOURCES[i]);
+					if(paths.length > 0){
+						for(var i = 0; i < paths.length; i++){
+							var f = ['src', '/ios/', paths[i]].join('');
+							pathContents.push(TEMPLATE.replace('{{path}}', f));
+						}
+					}
+				}
+			}
+		}	
+	}
+	
+	var pluginContent = fs.readFileSync(pluginXml, {encoding: 'utf8'});
+	if(pluginContent.indexOf('<!--resources-->') >= 0 && pluginContent.indexOf('<!--endresources-->') >= 0){
+		var contentStart = [pluginContent.split('<!--resources-->')[0], '<!--resources-->'].join('');
+		var contentEnd = [pluginContent.split('<!--endresources-->')[1], '<!--endresources-->'].join('');
+		pluginContent = [contentStart, pathContents.join('\n'), contentEnd].join('\n');
+	}
+	fs.writeFileSync(pluginXml, pluginContent);
 };
